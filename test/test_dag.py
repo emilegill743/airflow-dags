ca7211597fc8e588bdb764f2a6cc3a184262e67f
emilegill743 <58830015+emilegill743@users.noreply.github.com>
2021-04-15 17:29:15 +0100

    Adding test email DAG

---
+++ test/test_dag.py
@@ -0,0 +1,23 @@
+from airflow.models import DAG
+from airflow.operators.email_operator import EmailOperator
+
+default_arguments = {
+    'owner': 'emilegill743',
+    'email': 'emilegill743@hotmail.com',
+    'start_date': datetime(2021, 4, 14),
+    'schedule_interval': @daily,
+    'retries': 3
+}
+
+test_email_dag = DAG(
+    dag_id='test_email_dag',
+)
+
+email_task = EmailOperator(
+    task_id='test_email',
+    to='emilegill743@hotmail.com',
+    subject='Airflow Test',
+    html_content='<title>This is a Test</title>',
+    dag=test_email_dag
+)
+
